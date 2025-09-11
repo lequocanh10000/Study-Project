b05abab2493ebbe5adfb9128210d46e80161c027
Tomoriu Akarin <lequocanh10000@github.com>
2025-09-11 16:01:15 +0700

    feat: add guard of role

---
+++ src/modules/auth/guards/role.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(private roles: string[]) {}
+    canActivate(
+        context: ExecutionContext
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        return this.roles.includes(request.user.role);
+    }
+}
