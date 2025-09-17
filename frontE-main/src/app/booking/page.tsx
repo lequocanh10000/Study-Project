ca647940897b133bd23990dc599c2cebf73181ce
Tomoriu Akarin <lequocanh10000@github.com>
2025-09-17 15:19:34 +0700

    feat: delete

--- frontE-main/src/app/booking/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { useState } from 'react';
-import FlightBookingApp from '../../components/flight/FlightBooking/flightBooking';
-
-export default function BookingPage() {
-  return <FlightBookingApp />;
-}
